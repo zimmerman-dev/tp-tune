be16f741c37a36f650ce612095f954059e51c7d2
John Zimmerman <zimmerman-dev@outlook.com>
2025-11-27 20:02:29 -0600

    testing query function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+bool userQuery() {
+  std::cout << "Enter a 'y' for yes, 'n' for no: ";
+
+  while (true) {
+    char input{};
+    std::cin >> input;
+
+    if (input == 'y' || input == 'Y') {
+      std::cout << "You entered yes!\n";
+      return true;
+    }
+    else if (input == 'n' || input == 'N') {
+      std::cout << "You entered no...\n";
+      return false;
+    }
+    else {
+      std::cout << "Try again!: ";
+    }
+  }
+}
+
 int main() {
-    std::cout << "Hello from cpp-template!" << std::endl;
-    return 0;
+  bool query{userQuery()};
+
+  return 0;
 }
